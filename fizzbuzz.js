6e2c8741584143cdd73f311a2fd924be99f941bb
Joey Gold <joey.gold@softwire.com>
2025-04-22 15:42:50 +0100

    Bang

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,25 @@
 function fizzBuzz() {
-    for (let number =1; number <= 100; number++) {
+    for (let number = 1; number <= 100; number++) {
         console.log(getOutputForNumber(number));
     }
 }
 
 function getOutputForNumber(number) {
     let output = '';
-    if (number % 3 === 0) {
-        output += 'Fizz';
-    }
-    if (number % 5 === 0) {
-        output += 'Buzz';
-    }
+    output += getAdditionForNumber(number, 3, 'Fizz');
+    output += getAdditionForNumber(number, 5, 'Buzz');
+    output += getAdditionForNumber(number, 7, 'Bang');
     if (output.length > 0) {
         return output;
     }
     return number;
+}
+
+function getAdditionForNumber(number, divisor, word) {
+    if (number % divisor === 0) {
+        return word;
+    }
+    return '';
 }
 
 fizzBuzz();

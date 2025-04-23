ad0826d00f8f350b1150200136d07ceb90da721d
Joey Gold <joey.gold@softwire.com>
2025-04-23 16:12:25 +0100

    Add reverse rule

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,33 +1,42 @@
 function fizzBuzz() {
-    for (let number = 1; number <= 250; number++) {
+    for (let number = 1; number <= 256; number++) {
         console.log(getOutputForNumber(number));
     }
 }
 
 function getOutputForNumber(number) {
-    let output = '';
-    
-    output = applySimpleRule(output, number, 3, 'Fizz');
-    output = applySimpleRule(output, number, 13, 'Fezz');
-    output = applySimpleRule(output, number, 5, 'Buzz');
-    output = applySimpleRule(output, number, 7, 'Bang');
+    let output = [];
+    output = applySimpleRule(output, number, 3, "Fizz");
+    output = applySimpleRule(output, number, 13, "Fezz");
+    output = applySimpleRule(output, number, 5, "Buzz");
+    output = applySimpleRule(output, number, 7, "Bang");
     output = bongRule(output, number);
+    output = reverseRule(output, number);
     if (output.length > 0) {
-        return output;
+        return output.join("");
     }
     return number;
 }
 
 function applySimpleRule(currentOutput, number, divisor, word) {
     if (number % divisor === 0) {
-        return currentOutput + word;
+        currentOutput.push(word);
+        return currentOutput;
     }
     return currentOutput;
 }
 
 function bongRule(currentOutput, number) {
     if (number % 11 === 0) {
-        return number % 13 === 0 ? "FezzBong" : "Bong";
+        currentOutput.filter(word => word === "Fezz").push("Bong");
+        return currentOutput;
+    }
+    return currentOutput;
+}
+
+function reverseRule(currentOutput, number) {
+    if (number % 17 === 0) {
+        return currentOutput.reverse();
     }
     return currentOutput;
 }
